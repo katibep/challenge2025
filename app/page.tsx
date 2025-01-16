fd06e4e1eeb72c413b5ffd29950ca4c86fb3da2f
unknown <katibepatience@gmail.com>
2025-01-16 13:51:36 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
 import React from 'react'
+
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='pt-[3rem]'>
+      <div className="flex items-center justify-center gap-[2rem]">
+        <Input className='w-[50%]' placeholder='search...'/>
+        <Search/>
+      </div>
+    
+    </div>
   )
 }
 
