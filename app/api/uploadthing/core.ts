e9690e1da220093d6f0487ead668565e41e59cd7
unknown <katibepatience@gmail.com>
2025-01-29 20:22:53 +0300

    fir

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -6,7 +6,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  categoryImage: f({ image: { maxFileSize: "1MB" } }).onUploadComplete(
+  categoryImage: f({ image: { maxFileSize: "4MB" } }).onUploadComplete(
     async ({ metadata, file }) => {
       console.log("file url", file.url);
       return { uploadedBy: "ie" };
